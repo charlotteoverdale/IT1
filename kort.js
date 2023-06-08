18e6acdb6fc64c8646914fe7cb9b25c92237ba37
charlotteoverdale <113419423+charlotteoverdale@users.noreply.github.com>
2023-06-08 18:38:41 +0200

    Add files via upload

---
+++ kort.js
@@ -0,0 +1,31 @@
+// Eksempeldata for personinformasjon
+var dataInfo = {
+    "kortArray": [
+        {
+            navn: "Velg person",
+        },
+        {
+            navn: "Charlotte",
+            kjonn: "Kvinne",
+            stilling: "Jobber på parfymeri",
+            telefonnummer: "93095009",
+            valgfri: "Jeg har to katter og en hund",
+            forerkort: false,
+            fodselsdag: "11.08.2005",
+            bilde: "bilde1.jpeg"
+        },
+        {
+            navn: "Noah",
+            kjonn: "Mann",
+            stilling: "Spiller håndball",
+            telefonnummer: "90609278",
+            valgfri: "Jeg har spilt håndball siden jeg var 2",
+            forerkort: true,
+            fodselsdag: "28.03.2004",
+            bilde: "bilde2.jpg"
+        }
+        // Legg til flere personer etter behov
+    ]
+};
+
+
