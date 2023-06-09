539ac8a039ab401944363ec29c482194c097bbe3
charlotteoverdale <113419423+charlotteoverdale@users.noreply.github.com>
2023-06-09 09:35:09 +0200

    Add files via upload

--- kort.js
+++ kort.js
@@ -23,6 +23,36 @@ var dataInfo = {
             forerkort: true,
             fodselsdag: "28.03.2004",
             bilde: "bilde2.jpg"
+        },
+        {
+            navn: "Thomas",
+            kjonn: "Mann",
+            stilling: "Jobber som flygeleder",
+            telefonnummer: "90024944",
+            valgfri: "Jeg lærte norsk når jeg var 6",
+            forerkort: true,
+            fodselsdag: "26.05.1972",
+            bilde: "bilde3.jpeg"
+        },
+        {
+            navn: "Madeleine",
+            kjonn: "Kvinne",
+            stilling: "Studerer industriell design",
+            telefonnummer: "93044009",
+            valgfri: "Jeg har bodd i trondheim i to år nå",
+            forerkort: true,
+            fodselsdag: "03.03.2002",
+            bilde: "bilde4.jpeg"
+        },
+        {
+            navn: "Josephine",
+            kjonn: "Kvinne",
+            stilling: "Spiller fotball",
+            telefonnummer: "93054009",
+            valgfri: "Jeg er den yngste av tre søsken",
+            forerkort: false,
+            fodselsdag: "15.01.2012",
+            bilde: "bilde5.jpeg"
         }
         // Legg til flere personer etter behov
     ]
